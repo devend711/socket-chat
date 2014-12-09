6e53caf6f08df9f47c843145d97646fe233e0789
Deven <devend711@gmail.com>
2014-12-09 16:12:31 -0500

    add usernames

--- public/chat.js
+++ public/chat.js
@@ -4,24 +4,30 @@ window.onload = function() {
  
     var messages = [];
     var socket = io.connect('http://localhost:3700');
-    var field = $("#text-box.field");
+    var field = $('#text-box');
     var sendButton = $('input#send-button');
-    var content = $("#content");
+    var content = $('#content');
+    var name = $('#name');
  
     socket.on('message', function (data) { // bind a function, expect an object named data with a property 'message'
         if(data.message) {
             console.log('socket received message: ' + data.message)
             console.log('there are now ' + messages.length + ' messages')
             messages.push(data.message);
-            content.append(data.message + '<br/>');
+            content.append(data.username + ': ' + data.message + '<br/>');
         } else {
-            console.log("There is a problem:", data);
+            console.log('error!: ', data);
         }
     });
  
     sendButton.click(function() {
         var text = field.val();
-        socket.emit('send', { message: text }); // fire a 'send' event to the socket
+        var username = name.val();
+        if(username == "") {
+            alert("Pick a username!");
+        } else {
+            socket.emit('send', { message: text, username: username }); // fire a 'send' event to the socket
+        }
     });
  
 }
